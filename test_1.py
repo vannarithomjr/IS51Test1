bc4e2e4ebd7d4fafe18165545b55c08393674d33
Vannarith Om Jr <vannarithomjr@swuser-129-8-212-104.swlan.csufresno.edu>
2022-03-24 18:50:24 -0700

    Pseudocode done

--- test_1.py
+++ test_1.py
@@ -9,3 +9,34 @@ The option2 is going to use exponential. That would be 2^9. we set 2 because the
 of 10 because we discount the first day with $1. After we get both of the final amounts, we use another function to
 compare which value es higher and display a message stating which one is more convenient.
 """
+
+
+
+# Pseudocode:
+"""
+option1
+	return 100*days
+
+option2
+	return 2^(days - 1)
+
+calculations
+	if salary1 > salary2 then
+		print option1 is better
+
+	if salary1 = salary2 then
+		print both options are the same
+
+	else then
+		print option2 is better
+
+main
+	set days to 10
+	set salary1 to the option1 function
+	set salary2 to the option2 function
+	print the value of salary1
+	print the value of salary2
+	call the calculations function
+
+call for the main function
+"""
