8bd2e671e899726d9c09e8c8aed6615aa745971d
Vannarith Om Jr <vannarithomjr@swuser-129-8-214-193.swlan.csufresno.edu>
2022-03-25 13:33:28 -0700

    Coding Porting finished

--- test_1.py
+++ test_1.py
@@ -5,7 +5,8 @@
 Even though there is mulltiple ways to solve question 1, one of the most easiest ways to do it is by creating
 a function for each of the two possibles salaries. That is two functions labeled as option1 and option2.
 option 1 is going to directly multiply the amount of money per day by the days worked. That is $100x10 = $1,000.
-The option2 is going to use exponential. That would be 2^9. we set 2 because the amount doubles, and we set 9 instead
+The option2 is going to double the number (starting with 1) and is going to append it at the end once the 10 days have passed.
+That would be the sum of [1, 2, 4, 8, 16, 32, 64, 128, 256, 512], which is 1023. We set 9 instead
 of 10 because we discount the first day with $1. After we get both of the final amounts, we use another function to
 compare which value es higher and display a message stating which one is more convenient.
 """
@@ -18,7 +19,13 @@ option1
 	return 100*days
 
 option2
-	return 2^(days - 1)
+	set an empty arr
+	set number to 1
+	append the value of number to arr
+	for x in range of 9
+		number = number + number
+		append number to arr
+	return sum of array
 
 calculations
 	if salary1 > salary2 then
