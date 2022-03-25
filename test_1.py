0c91520a5a52d8d5282f1dd1f567d1ad805d804a
Vannarith Om Jr <vannarithomjr@swuser-129-8-212-104.swlan.csufresno.edu>
2022-03-24 18:46:30 -0700

    Structured English Portion done.

---
+++ test_1.py
@@ -0,0 +1,11 @@
+# Test 1 - Samuel Vannarith Om Mercado
+
+# Structured English:
+"""
+Even though there is mulltiple ways to solve question 1, one of the most easiest ways to do it is by creating
+a function for each of the two possibles salaries. That is two functions labeled as option1 and option2.
+option 1 is going to directly multiply the amount of money per day by the days worked. That is $100x10 = $1,000.
+The option2 is going to use exponential. That would be 2^9. we set 2 because the amount doubles, and we set 9 instead
+of 10 because we discount the first day with $1. After we get both of the final amounts, we use another function to
+compare which value es higher and display a message stating which one is more convenient.
+"""
